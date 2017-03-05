ebcab8f9e98caf9c2301d041fd2cf3e196dfa7e2
vanessamuller <vane9090@gmail.com>
2017-03-04 19:53:23 -0500

    added show method, added find_pin method and before_action to pins controller and created show view for pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,10 @@
 class PinsController < ApplicationController
+  before_action :find_pin, :only[:show, :edit, :update, :destroy]
+
   def index
+  end
+
+  def show
   end
 
   def new
@@ -20,5 +25,9 @@ class PinsController < ApplicationController
 
   def pin_params
     params.require(:pin).permit(:title, :description)
+  end
+
+  def fin_pin
+    @pin = Pin.find(params[:id])
   end
 end
