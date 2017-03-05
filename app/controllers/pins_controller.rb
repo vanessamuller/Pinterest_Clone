6af7c7aaa80a85cfb9af726cd1f772e1e9fccf17
vanessamuller <vane9090@gmail.com>
2017-03-04 19:21:52 -0500

    added methods to Pins controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -9,4 +9,10 @@ class PinsController < ApplicationController
   def create
     @pin = Pin.new(pin_params)
   end
+
+  private
+
+  def pin_params
+    params.require(:pin).permit(:title, :description)
+  end
 end
